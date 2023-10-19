98052b4d4dc4b5661efd648abea8a90111295329
mohamedhk2 <mohamedhk2@yahoo.fr>
2023-10-19 01:10:30 +0100

    refactor functions.php

--- wp-rocket-activator.php
+++ wp-rocket-activator.php
@@ -5,21 +5,22 @@
  * Plugin Name:       WP Rocket Activator
  * Plugin URI:        https://github.com/wp-activators/wp-rocket-activator
  * Description:       WP Rocket Plugin Activator
- * Version:           1.0.0
- * Requires at least: 3.1.0
+ * Version:           1.1.0
+ * Requires at least: 5.9.0
  * Requires PHP:      7.2
  * Author:            mohamedhk2
  * Author URI:        https://github.com/mohamedhk2
  **/
 
 defined( 'ABSPATH' ) || exit;
-const WP_ROCKET_ACTIVATOR_NAME   = 'WP Rocket Activator';
-const WP_ROCKET_ACTIVATOR_DOMAIN = 'wp-rocket-activator';
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+$WP_ROCKET_ACTIVATOR_NAME   = 'WP Rocket Activator';
+$WP_ROCKET_ACTIVATOR_DOMAIN = 'wp-rocket-activator';
+$functions                  = require_once __DIR__ . DIRECTORY_SEPARATOR . 'functions.php';
+extract( $functions );
 if (
-	activator_admin_notice_ignored()
-	|| activator_admin_notice_plugin_install( 'wp-rocket/wp-rocket.php', null, 'WP Rocket', WP_ROCKET_ACTIVATOR_NAME, WP_ROCKET_ACTIVATOR_DOMAIN )
-	|| activator_admin_notice_plugin_activate( 'wp-rocket/wp-rocket.php', WP_ROCKET_ACTIVATOR_NAME, WP_ROCKET_ACTIVATOR_DOMAIN )
+	$activator_admin_notice_ignored()
+	|| $activator_admin_notice_plugin_install( 'wp-rocket/wp-rocket.php', null, 'WP Rocket', $WP_ROCKET_ACTIVATOR_NAME, $WP_ROCKET_ACTIVATOR_DOMAIN )
+	|| $activator_admin_notice_plugin_activate( 'wp-rocket/wp-rocket.php', $WP_ROCKET_ACTIVATOR_NAME, $WP_ROCKET_ACTIVATOR_DOMAIN )
 ) {
 	return;
 }
@@ -31,6 +32,7 @@ set_transient( 'wp_rocket_customer_data', $wp_rocket_customer_data = (object) [
 	'has_one-com_account' => false,
 	'licence_expiration'  => strtotime( '+1000 year' ),
 ], 1000 * YEAR_IN_SECONDS );
+set_transient( 'rocket_analytics_optin', 0, 1000 * YEAR_IN_SECONDS );
 
 $rocket_boxes = (array) get_user_meta( get_current_user_id(), 'rocket_boxes', true );
 if ( ! in_array( 'rocket_activation_notice', $rocket_boxes, true ) ) {
@@ -39,13 +41,19 @@ if ( ! in_array( 'rocket_activation_notice', $rocket_boxes, true ) ) {
 }
 add_action( 'plugins_loaded', function () use ( $consumer_email ) {
 	update_rocket_option( 'secret_key', hash( 'crc32', $consumer_email ) );
+	update_rocket_option( 'analytics_enabled', 0 );
 }, 99 );
 
 use WP_Rocket\Engine\License\API\UserClient;
 
-add_filter( 'pre_http_request', function ( $pre, $parsed_args, $url ) use ( $wp_rocket_customer_data ) {
-	if ( $url == UserClient::USER_ENDPOINT ) {
-		return activator_json_response( $wp_rocket_customer_data );
+add_filter( 'pre_http_request', function ( $pre, $parsed_args, $url ) use ( $wp_rocket_customer_data, $activator_json_response ) {
+	switch ( $url ) {
+		case UserClient::USER_ENDPOINT:
+			return $activator_json_response( $wp_rocket_customer_data );
+		case WP_ROCKET_WEB_API . 'pause-licence.php':
+		case WP_ROCKET_WEB_API . 'activate-licence.php':
+			return $activator_json_response( [] );
+
 	}
 
 	return $pre;
